14e4e5adb6bd8c01208005e371f610823c2d8f11
Dacendaran <57763867+Dacendaran@users.noreply.github.com>
2023-11-10 16:19:27 +0100

    Improved ReflectionUtils

--- Runtime/ReflectionUtils.cs
+++ Runtime/ReflectionUtils.cs
@@ -8,16 +8,32 @@ namespace Dacen.Utils.Reflection
     public static class ReflectionUtils
     {
         /// <summary>
-        /// Gets every type deriving from this type
+        /// Gets every type directly deriving from a type
         /// </summary>
         /// <param name="type">The type to get the deriving types for</param>
-        /// <param name="baseTypeInclusive">Should the base type (the type this method is executed for) be included in the returned IEnumerable?</param>
-        /// <returns>Every type deriving from the type this method is executed for</returns>
-        public static IEnumerable<Type> GetDerivingTypes(Type type, bool baseTypeInclusive = false)
+        /// <param name="assembly">The assembly to search for deriving types in. If null, <see cref="Assembly.GetExecutingAssembly"/> is used</param>
+        /// <param name="baseTypeInclusive">Should the passed base type be included in the returned IEnumerable?</param>
+        /// <returns>Every type directly deriving from the passed type</returns>
+        public static IEnumerable<Type> GetDerivingTypes(Type type, Assembly assembly = null, bool baseTypeInclusive = false)
         {
-            if (baseTypeInclusive)
-                return Assembly.GetExecutingAssembly().GetTypes().Where(t => type.IsAssignableFrom(t));
-            return Assembly.GetExecutingAssembly().GetTypes().Where(t => t != type && type.IsAssignableFrom(t));
-        }
+            assembly ??= Assembly.GetExecutingAssembly();
+            var result = assembly.GetTypes().Where(t => type.IsAssignableFrom(t));
+            return baseTypeInclusive ? result : result.Where(t => t != type);
+		}
+
+		/// <summary>
+		/// Gets every type directly deriving from a generic type
+		/// </summary>
+		/// <param name="genericType">The generic type to get the deriving types for (pass it as typeof(GenericElement<>)</param>
+		/// <param name="assembly">The assembly to search for deriving types in. If null, <see cref="Assembly.GetExecutingAssembly"/> is used</param>
+		/// <param name="baseTypeInclusive">Should the passed base type be included in the returned IEnumerable?</param>
+		/// <returns>Every type directly deriving from the passed type</returns>
+		public static IEnumerable<Type> GetGenericDerivingTypes(Type genericType, Assembly assembly = null, bool baseTypeInclusive = false)
+        {
+			assembly ??= Assembly.GetExecutingAssembly();
+            var result = assembly.GetTypes()
+	            .Where(t => t.BaseType?.IsGenericType == true && t.BaseType.GetGenericTypeDefinition() == genericType);
+			return baseTypeInclusive ? result : result.Where(t => t != genericType);
+		}
     }
 }
